4cb7eced83c21d894d42cc92d7c82a6be74286b4
Andrew Tiller <tiller.andrew@gmail.com>
2019-05-15 20:14:20 -0500

    currently printing categories with types

---
+++ src/javascripts/components/products/products.js
@@ -0,0 +1,30 @@
+// import util from '../../helpers/util';
+import categoryData from '../../helpers/data/categoriesData';
+import typeData from '../../helpers/data/typesData';
+
+// const writeProducts = (products) => {
+//   let domString = '';
+//   products.forEach((product) => {
+//     domString += '<div class="col-3">';
+//     domString += `<div id='${product.id}' class="card p-2">`;
+//     domString += '<div class="card-body">';
+//     domString += `<h5 class="card-title">${product.name}</h5>`;
+//     domString += `<button class="btn btn-warning see-pins">${product.pins.length} Pins</button>`;
+//     domString += '</div>';
+//     domString += '</div>';
+//     domString += '</div>';
+//   });
+//   util.printToDom('products', domString);
+// };
+
+const initProducts = () => {
+  categoryData.loadCategories()
+    .then(resp => console.error(typeData.getTypesForEachCategory(resp.data.categories)))
+    // .then(resp => pinsData.getPinsForEachBoard(resp.data.boards))
+    // .then((boardsWithPins) => {
+    //   writeProducts(boardsWithPins);
+    // })
+    .catch(err => console.error('error from initBoards requests', err));
+};
+
+export default { initProducts };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,10 @@
 import 'bootstrap';
 import '../styles/main.scss';
+import products from './components/products/products';
 
 const init = () => {
   console.error('hello from main.js');
+  products.initProducts();
 };
 
 init();
